b30c7a620114bdb45195dd9b6879d2b551876702
MuizU <muizuvais99@gmail.com>
2023-04-06 15:18:51 +0530

    Added auth dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ParseIntPipe } from '@nestjs/common';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
-    console.log({ dto });
+  signup(@Body() dto: AuthDto) {
+    const { email, password } = dto;
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      typeofPwd: typeof password,
+      password,
+    });
     return this.authService.signup();
   }
   @Post('signin')
